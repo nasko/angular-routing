5e2d32b18f4efc0c55c80cf5170c2c4260e86732
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-11 23:39:14 +0300

    Protect all the child routes and not the root path

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,14 +1,14 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(router: ActivatedRouteSnapshot,
+  canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
     return this.authService.isAuthenticated()
@@ -21,6 +21,12 @@ export class AuthGuard implements CanActivate {
           }
         }
       );
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    return this.canActivate(route, state);
   }
 
 }
