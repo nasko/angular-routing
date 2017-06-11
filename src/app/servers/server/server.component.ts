e9d30a89e518f46622c9bf7b5c6fe459f2c516cc
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-11 22:47:52 +0300

    Preserving query params when navigating to child routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -11,7 +11,11 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
     let serverId = +this.route.snapshot.params['id'];
@@ -28,4 +32,8 @@ export class ServerComponent implements OnInit {
     );
   }
 
+  onEdit() {
+    // this.router.navigate(['servers', this.server.id, 'edit']);
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
+  }
 }
