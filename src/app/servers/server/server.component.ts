5efd5f18a44cbeeafacee041ffd75a08037374eb
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-06-12 23:28:58 +0300

    Use a route Resolver to provide dynamic data to the component via the Route

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ServersService} from '../servers.service';
+import {ActivatedRoute, Data, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,27 +9,31 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
 
-  constructor(
-    private serversService: ServersService,
-    private route: ActivatedRoute,
-    private router: Router,
-  ) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute,
+              private router: Router,) {
+  }
 
   ngOnInit() {
-    let serverId = +this.route.snapshot.params['id'];
-
-    if (!serverId) {
-      serverId = this.serversService.getServers()[0].id;
-    }
-    this.server = this.serversService.getServer(serverId);
-
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
       }
     );
+    // let serverId = +this.route.snapshot.params['id'];
+    //
+    // if (!serverId) {
+    //   serverId = this.serversService.getServers()[0].id;
+    // }
+    // this.server = this.serversService.getServer(serverId);
+    //
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   }
+    // );
   }
 
   onEdit() {
